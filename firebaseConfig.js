2db8ac0a29ed0ede3801e96bea71239eb3991950
extmnick-commits <extmnick@gmail.com>
2026-03-17 09:59:50 -0700

    Secure API keys and move to environment variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,16 @@
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from "firebase/app";
-import { getReactNativePersistence, initializeAuth, indexedDBLocalPersistence } from "firebase/auth";
+import { getReactNativePersistence, indexedDBLocalPersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { Platform } from 'react-native';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIYsTWO-GGmkNlpALCaoCTAE_TDkoHoYI",
-  authDomain: "estevanmytrackerapp.firebaseapp.com",
-  projectId: "estevanmytrackerapp",
-  storageBucket: "estevanmytrackerapp.firebasestorage.app",
-  messagingSenderId: "578894218502",
-  appId: "1:578894218502:web:15ee3a1cef18bf96ff0a87"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
